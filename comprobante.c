d6e206fff4078ddde548ee4026d358ee85d56db3
esteban <istico92@gmail.com>
2017-11-23 12:38:24 +0100

    Modificación del comentario

--- comprobante.c
+++ comprobante.c
@@ -6,30 +6,6 @@
 #include<stdio.h>
 int nif(int*x);
 int romanos(int*j);
-/*int comprobante(int*z, int*a)
-{
-
-    if (*z == 1)
-    {
-        if (( *a > 0) && ( *a <= 99999999))
-        {
-            nif(&*a);
-        }
-        else{
-            printf("Introduzca un valor comprendido entre 00000001 y 99999999");
-        }
-    }
-    if (*z == 2)
-    {
-        if ((( *a > 0) && ( *a <= 3999))  )
-        {
-            romanos(&*a);
-        }
-        else{
-            printf("Introduzca un número entero válido comprendido entre 1 y 3999");
-        }
-    }
-}*/
 int comprobante(int*z, int*a)
 {
 
